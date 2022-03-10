4941f07ca473ee0a5c892775e6f95dede4db7531
Sanusi Abdulkadir <sanusiabdulkadir00@gmail.com>
2022-03-10 23:41:18 +0100

    Updates

---
+++ precache-manifest.3a3344bca004ddc8f7dab9639218548a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb9cd6c83ab345c37102a81fd58b5088",
+    "url": "/ramadan-countdown-app/index.html"
+  },
+  {
+    "revision": "e60ddc8f320d5736463f",
+    "url": "/ramadan-countdown-app/static/css/main.3ad6f247.chunk.css"
+  },
+  {
+    "revision": "f968ac1269f24b2f1042",
+    "url": "/ramadan-countdown-app/static/js/2.729233ee.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ramadan-countdown-app/static/js/2.729233ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e60ddc8f320d5736463f",
+    "url": "/ramadan-countdown-app/static/js/main.a07295b6.chunk.js"
+  },
+  {
+    "revision": "571a4c3c0e1864f02ba9",
+    "url": "/ramadan-countdown-app/static/js/runtime-main.8795ae69.js"
+  }
+]);
