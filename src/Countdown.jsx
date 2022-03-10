fd8193fab3270d54612ccb69e14106a7220258a0
Sanusi Abdulkadir <94573138+sanusisusi@users.noreply.github.com>
2022-03-11 00:38:37 +0100

    Add files via upload

---
+++ src/Countdown.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Countdown = ({ countdownData, name }) => {
+  if (!countdownData.isItBday) {
+    return (
+      <div>
+        <h1 className='heading'>
+          Countdown to Ramadan 2022
+        </h1>
+        <div className='countdown-wrapper'>
+          <div className='countdown-box'>
+            {countdownData.days}
+            <span className='legend'>Days</span>
+          </div>
+          <div className='countdown-box'>
+            {countdownData.hours}
+            <span className='legend'>Hours</span>
+          </div>
+          <div className='countdown-box'>
+            {countdownData.minutes}
+            <span className='legend'>Minutes</span>
+          </div>
+          <div className='countdown-box'>
+            {countdownData.seconds}
+            <span className='legend'>Seconds</span>
+          </div>
+        </div>
+      </div>
+    );
+  }
+};
+
+export default Countdown;
